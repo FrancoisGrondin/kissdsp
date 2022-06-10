fcc6c74549ed741769e3453ec8eb910bd1bfcc40
François Grondin <Francois.Grondin2@USherbrooke.ca>
2022-06-10 16:31:47 -0400

    Added example with reverberation

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
-import argparse
+import argparse as ap
+import numpy as np
 import kissdsp.filterbank as fb
+import kissdsp.mixing as mx
+import kissdsp.reverb as rb
 import kissdsp.source as src
 import kissdsp.visualize as vz
 
@@ -14,10 +17,25 @@ def demo_spectrogram():
 	Ss = fb.stft(ss)
 	vz.spex(Ss)
 
+def demo_reverb():
+
+	rm = rb.room(mics=np.asarray([[-0.05, -0.05, +0.00], [-0.05, +0.05, +0.00], [+0.05, -0.05, +0.00], [+0.05, +0.05, +0.00]]),
+	             box=np.asarray([10.0, 10.0, 2.5]),
+	             srcs=np.asarray([[2.0, 3.0, 1.0]]),
+	             origin=np.asarray([4.0, 5.0, 1.25]),
+	             alphas=0.5 * np.ones(6),
+	             c=343.0)
+	hs = rb.rir(rm)
+
+	ss = src.read("audio/speeches.wav")
+	xs = mx.collapse(rb.conv(hs, ss))
+	vz.wave(xs)
+
+
 def main():
 
-	parser = argparse.ArgumentParser(description='Choose demo.')
-	parser.add_argument('--operation', choices=['waveform', 'spectrogram'])
+	parser = ap.ArgumentParser(description='Choose demo.')
+	parser.add_argument('--operation', choices=['waveform', 'spectrogram', 'reverb'])
 	args = parser.parse_args()
 
 	if args.operation == 'waveform':
@@ -25,6 +43,9 @@ def main():
 
 	if args.operation == 'spectrogram':
 		demo_spectrogram()
+
+	if args.operation == 'reverb':
+		demo_reverb()
 
 if __name__ == "__main__":
 	main()
