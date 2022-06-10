dd875041f5b8f354fdbbe47283e4325c8f7f79f2
François Grondin <Francois.Grondin2@USherbrooke.ca>
2022-06-10 16:01:06 -0400

    Added demo to display waveforms

--- demo.py
+++ demo.py
@@ -1,1 +1,22 @@
+import kissdsp.source as src
+import kissdsp.visualize as vz
+
+def demo_waveform():
+
+	ss = src.read("audio/speeches.wav")
+	vz.wave(ss)
+
+def demo_spectrogram():
+
+	ss = src.read("audio/speeches.wav")
+
+def main():
+
+	demo_waveform()
+
+if __name__ == "__main__":
+	main()
+
+
+
 
