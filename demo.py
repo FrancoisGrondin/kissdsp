Fix bug with masking

demo.py
@@ -86,10 +86,34 @@ def demo_mask():
 	Ys = fb.stft(ys)
 
 	# Compute masks
-	Ms = mk.irm(Ts, Ys)
+	Ms = mk.ibm(Ts, Ys)
 
 	# Display
 	vz.mask(Ms)
+
+
+def demo_mvdr():
+
+	# Create a rectangular room with two sources
+	rm = rb.room(mics=np.asarray([[-0.05, -0.05, +0.00], [-0.05, +0.05, +0.00], [+0.05, -0.05, +0.00], [+0.05, +0.05, +0.00]]),
+	             box=np.asarray([10.0, 10.0, 2.5]),
+	             srcs=np.asarray([[2.0, 3.0, 1.0], [8.0, 7.0, 1.5]]),
+	             origin=np.asarray([4.0, 5.0, 1.25]),
+	             alphas=0.5 * np.ones(6),
+	             c=343.0)
+
+	# Create room impulse responses
+	hs = rb.rir(rm)
+
+	# Load speech audio
+	ss = src.read("audio/speeches.wav")
+
+	# Apply room impulse response
+	xs = rb.conv(hs, ss)
+
+	# Get target and interference
+	ts = mx.source(xs, 0)
+
 
 
 def main():

kissdsp/masking.py
@@ -1,40 +1,40 @@
 import numpy as np
 
 
-def irm(Ts, Ys):
+def irm(Ts, Is):
     """
     Generate Ideal Ratio Mask
 
     Args:
         Ts (np.ndarray):
             Frequency domain representation of the target (nb_of_channels, nb_of_frames, nb_of_bins).
-        Ys (np.ndarray):
-            Frequency domain representation of the mixture (nb_of_channels, nb_of_frames, nb_of_bins).
+        Is (np.ndarray):
+            Frequency domain representation of the interference (nb_of_channels, nb_of_frames, nb_of_bins).
 
     Returns:
         Ms (np.ndarray):
             Ideal ratio mask (nb_of_channels, nb_of_frames, nb_of_bins).
     """
 
-    return np.clip((np.abs(Ts) ** 2) / (np.abs(Ys) ** 2 + 1e-20), a_min=0.0, a_max=1.0)
+    return np.clip((np.abs(Ts) ** 2) / (np.abs(Ts) ** 2 + np.abs(Is) ** 2 + 1e-20), a_min=0.0, a_max=1.0)
 
 
-def ibm(Ts, Ys, threshold=0.0):
+def ibm(Ts, Is, threshold=0.0):
     """
     Generate Ideal Binary Mask
 
     Args:
         Ts (np.ndarray):
             Frequency domain representation of the target (nb_of_channels, nb_of_frames, nb_of_bins).
-        Ys (np.ndarray):
-            Frequency domain representation of the mixture (nb_of_channels, nb_of_frames, nb_of_bins).
+        Is (np.ndarray):
+            Frequency domain representation of the interference (nb_of_channels, nb_of_frames, nb_of_bins).
         threshold (float):
-            Threshold in dB when comparing with mixture (10 log(|Ts|**2) > 10 log(|Mx|**2) + threshold).
+            Threshold in dB when comparing with interference (10 log(|Ts|**2) > 10 log(|Is|**2) + threshold).
 
     Returns:
         (np.ndarray):
             Ideal binary mask (nb_of_channels, nb_of_frames, nb_of_bins).
     """
 
-    return (10.0 * np.log10(np.abs(Ts) ** 2)) > (10.0 * np.log10(np.clip(np.abs(Ys) ** 2 - np.abs(Ts) ** 2, a_min=1e-20, a_max=None)) + threshold)
+    return (10.0 * np.log10(np.clip(np.abs(Ts) ** 2, a_min=1e-20, a_max=None)) > (10.0 * np.log10(np.clip(np.abs(Is) ** 2, a_min=1e-20, a_max=None)) + threshold))
 